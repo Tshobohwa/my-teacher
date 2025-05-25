cf59a4b5b304d46c593e298c98d55ad07bc55cee
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-05-25 12:23:59 +0300

    Create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="underline text-2xl">Welcome to my Saas</h1>
-        <Button>Press me</Button>
-    </div>
+    <main>
+        <h1 className={"text-2xl underline"}>Popular Companions</h1>
+        <section className={"home-section"}>
+            <CompanionCard
+                id={1}
+                subject={"Science"}
+                duration={45  }
+                topic={"Neural Network of the Brain"}
+                name={"Neura the Brainy Explorer"}
+                color={"#E5D0FF"}
+            />
+            <CompanionCard
+                id={2}
+                subject={"Maths"}
+                duration={30}
+                topic={"Derivatives & Integrals"}
+                name={"Countsy the Numb er Wizard"}
+                color={'#FFDA6E'}
+            />
+            <CompanionCard
+                id={3}
+                subject={"Language"}
+                duration={30}
+                topic={"English and Literature"}
+                name={"Verba the vocabulary builder"}
+                color={"#BDE7FF"}
+            />
+        </section>
+        <section className={"home-section"}>
+            <CompanionsList
+                title={"Recently completed sessions"}
+                companions={recentSessions}
+                classNames={"w-2/3 max-lg:w-full"}
+            />
+            <CTA />
+        </section>
+    </main>
   )
 }
 
---
+++ components/CTA.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Cta = ()  => {
+    return <section className={"cta-section"}>
+        <div className={"cta-badge"}>
+            Start learning your way
+        </div>
+        <h2 className={"text-3xl font-bold"}>
+            Build and personalize Learning companion
+        </h2>
+        <p>Pick a name, subject, voice, & personality - and start learning through voice conversations that feel natural and fun.</p>
+        <Image src={"images/cta.svg"} alt={"call to action"} height={232} width={362} />
+        <button className={"btn-primary"}>
+            <Image src={"/icons/plus.svg"} alt={"plus"} height={12} width={12} />
+            <Link href={"/companions/new"}>
+                <p>Build a New Companion</p>
+            </Link>
+        </button>
+    </section>
+}
+
+export default Cta;
