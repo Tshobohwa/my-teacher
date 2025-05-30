c9a21c6f58461ad7a71562b488d61c436ad50a45
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-05-30 21:43:15 +0300

    Get companions

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,5 +1,34 @@
-const Page = () => {
-    return <div>Companions</div>
+import {getAllCompanions} from "@/lib/actions/companion.action";
+import CompanionCard from "@/components/CompanionCard";
+import {getSubjectColor} from "@/lib/utils";
+
+const CompanionsLibrary = async ({searchParams}) => {
+    const filters = await searchParams;
+
+    const subject = filters.subject ? filters.subject : "";
+    const topic = filters.topic ? filters.topic : "";
+
+    const companions = await getAllCompanions({subject, topic});
+
+    console.log(companions);
+    return <main>
+        <section>
+            <h1>Companions Library</h1>
+            <div>Filters</div>
+        </section>
+        <section>
+            {companions.map(companion => <CompanionCard
+                id={companion.id}
+                name={companion.name}
+                topic={companion.topic}
+                subject={companion.subject}
+                duration={companion.duration}
+                color={getSubjectColor(companion.subject)}
+                key={companion.id}
+                />
+            )}
+        </section>
+    </main>
 }
 
-export default Page;
+export default CompanionsLibrary;
