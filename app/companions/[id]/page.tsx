0bcf9d7302762e88f4762c0f2005d4a0a6e7d199
Salomon Tshobohwa <stshobohwa@gmail.com>
2025-06-05 16:45:08 +0300

    Add image url

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import {currentUser} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
 import {getSubjectColor} from "@/lib/utils";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 
 interface CompanionSessionPageProps {
@@ -17,31 +18,39 @@ const CompanionSession = async ({params}: CompanionSessionPageProps) => {
     if (!user) redirect("/sign-in");
     if(!companion) redirect("/companions");
 
+    const {subject, name, duration, topic} = companion;
+
     // console.log(companion);
 
     return <main>
         <article className={"flex rounded-border justify-between p-6 max-md:flex-col"}>
             <div className={"flex items-center gap-2"}>
                 <div className={"size-[72px] flex items-center justify-center rounded-lg max-md:hidden"}
-                     style={{backgroundColor: getSubjectColor(companion?.subject)}}>
-                    <Image src={`/icons/${companion?.subject}.svg`} alt={companion?.subject} width={35} height={35} />
+                     style={{backgroundColor: getSubjectColor(subject)}}>
+                    <Image src={`/icons/${subject}.svg`} alt={subject} width={35} height={35} />
                 </div>
                 <div className={"flex flex-col gap-2"}>
                     <div className={"flex items-center gap-2"}>
                         <p className={"font-bold text-2xl"}>
-                            {companion?.name}
+                            {name}
                         </p>
                         <div className={"subject-badge max-sm:hidden"}>
-                            {companion?.subject}
+                            {subject}
                         </div>
                     </div>
-                    <p className={"text-lg"}>{companion?.topic}</p>
+                    <p className={"text-lg"}>{topic}</p>
                 </div>
                 <div className={"items-start text-2xl max-md:hidden"}>
-                    {companion?.duration} minutes
+                    {duration} minutes
                 </div>
             </div>
         </article>
+        <CompanionComponent
+            {...companion}
+            companionId={id}
+            userName={user.firstname!}
+            userImage={user.imageUrl}
+        />
     </main>
 }
 export default CompanionSession
